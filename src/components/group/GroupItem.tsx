a25a41f01334b6b8edb7dac71332c4fc21c9e0c6
cheonyeji <ssaltuck@gmail.com>
2023-07-08 17:52:19 +0900

    그룹색아이콘 안 줄어들게

--- src/components/group/GroupItem.tsx
+++ src/components/group/GroupItem.tsx
@@ -1,4 +1,3 @@
-import { useRef, useEffect } from "react";
 import { IGroup } from "../../models/atoms";
 import styled from "styled-components";
 
@@ -7,26 +6,10 @@ type IGroupItem = {
 };
 
 function GroupItem({ item }: IGroupItem) {
-  const itemRef = useRef<HTMLLIElement>(null);
-  // width 계산해서 circleDiv 안찌그러지게 꽉차게 하려고 했는데 잘 안되서 일단 고정 px값으로
-  // console.log(itemRef);
-  // let itemWidth = itemRef.current!.offsetWidth;
-  // let size: number = +window
-  //   .getComputedStyle(itemRef.current!, null)
-  //   .getPropertyValue("font-size");
-
-  // useEffect(() => {
-  //   itemWidth = itemRef.current!.offsetWidth;
-  //   // size = +window
-  //   //   .getComputedStyle(itemRef.current!, null)
-  //   //   .getPropertyValue("font-size");
-  // }, [itemRef.current]);
-
-  const textwidth = 150;
   return (
-    <Item ref={itemRef}>
+    <Item>
       <ColorCircle colorstring={item.color} />
-      <TextSpan textwidth={textwidth}>{item.title}</TextSpan>
+      {item.title}
     </Item>
   );
 }
@@ -40,13 +23,9 @@ const Item = styled.li`
 `;
 
 const ColorCircle = styled.div<{ colorstring: string }>`
-  width: 0.8em;
-  height: 0.8em;
+  min-width: 0.8em;
+  min-height: 0.8em;
   border-radius: 50%;
   background-color: ${(props) => props.colorstring};
   margin-right: 8px;
-`;
-
-const TextSpan = styled.span<{ textwidth: number }>`
-  width: ${(props) => props.textwidth}px;
 `;
