aa47813a8d4b04bed034b53e324f955e5c88b3dc
cheonyeji <ssaltuck@gmail.com>
2023-07-08 16:51:10 +0900

    날짜 선택가능한 캘린더 구현 - react-day-picker 사용 - 날짜는 selectedDateState atom값에 저장

---
+++ src/components/calendar/ShowCalendar.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { selectedDateState } from "../../models/atoms";
+
+const css = `
+  .my-selected {
+    background-color: #ABCBFC;
+    border-radius: 20%;
+    color: white;
+  }
+  .my-selected:hover:not([disabled]) { 
+    color: black;
+  }
+  .my-outside {
+    color: #ABCBFC;
+  }
+`;
+
+function ShowCalendar() {
+  const [pickedDate, setPickedDate] = useState<Date>();
+  const setselectedDateState = useSetRecoilState(selectedDateState);
+  const handleDayClick = (day: Date) => {
+    setPickedDate(day);
+    setselectedDateState(day);
+  };
+
+  return (
+    <div>
+      <style>{css}</style>
+      <DayPicker
+        mode="single" // single day can be selected
+        showOutsideDays // show outside dates
+        weekStartsOn={1} // start Mon
+        selected={pickedDate}
+        onDayClick={handleDayClick} // custom day select handler
+        // for CSS
+        modifiersClassNames={{
+          selected: "my-selected",
+          outside: "my-outside",
+        }}
+      />
+    </div>
+  );
+}
+
+export default ShowCalendar;
