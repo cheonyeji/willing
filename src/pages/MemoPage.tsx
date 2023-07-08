9d2c9d3e92fb55cc5bbea374dd8afcc42234b89e
cheonyeji <ssaltuck@gmail.com>
2023-07-08 16:59:14 +0900

    나에게보내는메모 페이지

---
+++ src/pages/MemoPage.tsx
@@ -0,0 +1,13 @@
+import Memos from "../components/memo/Memos";
+import NewMemo from "../components/memo/NewMemo";
+
+function MemoPage() {
+  return (
+    <div>
+      <Memos />
+      <NewMemo />
+    </div>
+  );
+}
+
+export default MemoPage;
