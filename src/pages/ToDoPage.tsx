25aeef315babf684bcd7fda503b1092439f24bf4
cheonyeji <ssaltuck@gmail.com>
2023-07-11 13:05:01 +0900

    페이지 상단(날짜, 날짜이동버튼) ToDoHeader 컴포넌트로 분리

--- src/pages/ToDoPage.tsx
+++ src/pages/ToDoPage.tsx
@@ -1,24 +1,13 @@
-import { useRecoilValue } from "recoil";
+import ToDoHeader from "../components/toDo/ToDoHeader";
 import NewToDo from "../components/toDo/NewToDo";
 import ToDos from "../components/toDo/ToDos";
-import { isSameDate, selectedDateState } from "../models/atoms";
-import { styled } from "styled-components";
 
 type ToDoPageProps = { className: string };
 
 function ToDoPage(props: ToDoPageProps) {
-  const selectedDate = useRecoilValue(selectedDateState);
-  const dateString = isSameDate(selectedDate, new Date())
-    ? "오늘의 할일"
-    : selectedDate.toLocaleDateString("ko-KR", {
-        // year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-
   return (
     <div className={props.className}>
-      <DateH2>{dateString}</DateH2>
+      <ToDoHeader />
       <NewToDo />
       <ToDos />
     </div>
@@ -26,10 +15,3 @@ function ToDoPage(props: ToDoPageProps) {
 }
 
 export default ToDoPage;
-
-const DateH2 = styled.h2`
-  font-size: 20px;
-  font-weight: 600;
-  line-height: 27px;
-  text-align: left;
-`;
