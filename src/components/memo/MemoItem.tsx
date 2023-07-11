d705a279bbfbc15d9b724dfd9f4aaa9402ccfff8
cheonyeji <ssaltuck@gmail.com>
2023-07-11 14:18:32 +0900

    나에게보내는채팅st 메모 아이템 - 시간 2자릿수로 고정 & ampm 포맷팅 - ui 반영

---
+++ src/components/memo/MemoItem.tsx
@@ -0,0 +1,53 @@
+import { styled } from "styled-components";
+import Memo from "../../models/memo";
+
+type MemoItemProps = {
+  item: Memo;
+};
+function MemoItem({ item }: MemoItemProps) {
+  const hours =
+    item.sendTime.getHours() % 12 ? item.sendTime.getHours() % 12 : 12;
+  const ampm = item.sendTime.getHours() >= 12 ? "PM" : "AM";
+  return (
+    <Li>
+      <ChatText>
+        <TextSpan>{item.text}</TextSpan>
+      </ChatText>
+      <TimeSpan>
+        {String(hours).padStart(2, "0") +
+          ":" +
+          String(item.sendTime.getMinutes()).padStart(2, "0") +
+          " " +
+          ampm}
+      </TimeSpan>
+    </Li>
+  );
+}
+
+export default MemoItem;
+
+const Li = styled.li`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const ChatText = styled.div`
+  background-color: #f0f5fc;
+  border-radius: 30px 30px 1px 30px;
+  margin: 8px;
+`;
+
+const TimeSpan = styled.span`
+  font-size: 10px;
+  margin-right: 5px;
+  margin: 8px;
+  color: #929292;
+`;
+
+const TextSpan = styled.span`
+  margin: 24px 27px;
+  word-break: break-all; // ~ 등의 문자 줄바꿈을 위해
+  display: inline-block;
+`;
