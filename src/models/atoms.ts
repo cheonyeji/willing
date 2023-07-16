b84994548007bdf9e7ea0daf490105a211b4f516
cheonyeji <ssaltuck@gmail.com>
2023-07-16 10:33:29 +0900

    그룹 생성 시 기본 제공되는 컬러칩 데이터 전역 state로 관리 - IColor interface 생성 - colorsState atom 생성 - id에 따라 color데이터 전달하는 selector 생성

--- src/models/atoms.ts
+++ src/models/atoms.ts
@@ -113,3 +113,31 @@ export const isOverTrashCanState = atom<boolean>({
   key: "isOverTrashCanState",
   default: false,
 });
+
+/* 디폴트 색상 배열 */
+export interface IColor {
+  id: number;
+  color: string;
+}
+
+export const colorsState = atom<IColor[]>({
+  key: "colorsState",
+  default: [
+    { id: 0, color: "#AEE4FF" },
+    { id: 1, color: "#e1aeff" },
+    { id: 2, color: "#6bd2bf" },
+    { id: 3, color: "#bb8787" },
+    { id: 4, color: "#98ae6e" },
+  ],
+});
+
+// return color Item (find by colorId). use selectorFamily for parameter
+export const colorItemByIdSelector = selectorFamily({
+  key: "colorItemByIdSelector",
+  get:
+    (colorId: number) =>
+    ({ get }) => {
+      const colors = get(colorsState);
+      return colors.find((item) => item.id === colorId)!;
+    },
+});
