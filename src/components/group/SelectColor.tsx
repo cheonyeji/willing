99e1ce1ec9ea6493104b9371f9636c40ac420623
cheonyeji <ssaltuck@gmail.com>
2023-07-16 10:28:27 +0900

    파일명변경 & 제네릭컴포넌트로 변경

--- src/components/group/SelectColor.tsx
+++ src/components/group/SelectColor.tsx
@@ -2,45 +2,48 @@ import { useState } from "react";
 import { styled } from "styled-components";
 import IconSelectUp from "../icons/IconSelectUp";
 import IconSelectDown from "../icons/IconSelectDown";
+import Dropdown from "../UI/Dropdown";
+import { IColor, colorItemByIdSelector, colorsState } from "../../models/atoms";
+import { useRecoilValue } from "recoil";
 
-type SelectDropdownProps = {
-  groupColorState: string;
-  setGroupColorState: (s: string) => void;
+type SelectColorProps = {
+  selectedColorIdState: number;
+  setSelectedColorIdState: (n: number) => void;
 };
 
-function SelectDropdown(props: SelectDropdownProps) {
-  const colorArr = ["#AEE4FF", "#e1aeff", "#6bd2bf", "#bb8787", "#98ae6e"];
-
+function SelectColor(props: SelectColorProps) {
+  const colors = useRecoilValue(colorsState);
   const [isUlVisible, setIsUlVisible] = useState(false);
-
+  const colorItemById = useRecoilValue(
+    colorItemByIdSelector(props.selectedColorIdState)
+  );
   const toggleSelect = () => {
     setIsUlVisible(!isUlVisible);
   };
+
   const liClickHandler = (event: React.MouseEvent<HTMLElement>) => {
-    props.setGroupColorState(event.currentTarget.id);
+    props.setSelectedColorIdState(+event.currentTarget.id);
     setIsUlVisible(!isUlVisible);
   };
   return (
     <Wrapper>
       <SelectBtn onClick={toggleSelect}>
-        <ColorCircle colorstring={props.groupColorState} />
+        <ColorCircle colorstring={colorItemById.color} />
         <IconWrapper>
           {isUlVisible ? <IconSelectUp /> : <IconSelectDown />}
         </IconWrapper>
       </SelectBtn>
-
-      <Ul visible={isUlVisible}>
-        {colorArr.map((item) => (
-          <Li key={item} id={item} onClick={liClickHandler}>
-            <ColorCircle colorstring={item} />
-          </Li>
-        ))}
-      </Ul>
+      <Dropdown<IColor>
+        isUlVisible={isUlVisible}
+        setIsUlVisible={setIsUlVisible}
+        liClickHandler={liClickHandler}
+        dataArray={colors}
+      />
     </Wrapper>
   );
 }
 
-export default SelectDropdown;
+export default SelectColor;
 
 const Wrapper = styled.div`
   margin-right: 5px;
@@ -65,25 +68,4 @@ const IconWrapper = styled.div`
   margin-left: 5px;
   display: flex;
   align-items: center;
-`;
-
-const Ul = styled.ul<{ visible: boolean }>`
-  display: ${(props) => (props.visible ? "block" : "none")};
-  list-style: none;
-  padding: 0;
-  position: absolute;
-  border-radius: 4px;
-  background-color: #ffffff;
-  box-shadow: 0px 8px 8px 0px rgba(29, 91, 132, 0.25);
-`;
-
-const Li = styled.li`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  &:hover {
-    background-color: #cae2fe9c;
-  }
-  padding: 8px;
-  border-radius: 2px;
 `;
--- src/components/toDo/SelectGroup.tsx
+++ src/components/toDo/SelectGroup.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { groupItemById, groupsState } from "../../models/atoms";
+import { IGroup, groupItemById, groupsState } from "../../models/atoms";
 import { styled } from "styled-components";
 import IconSelectDown from "../icons/IconSelectDown";
 import IconSelectUp from "../icons/IconSelectUp";
+import Dropdown from "../UI/Dropdown";
 
-type GroupSelectProps = {
+type SelectGroupProps = {
   selectedGroupId: number;
   setSelectedGroupId: (n: number) => void;
 };
 
-function GroupSelect(props: GroupSelectProps) {
+function SelectGroup(props: SelectGroupProps) {
   const groups = useRecoilValue(groupsState);
   const [isUlVisible, setIsUlVisible] = useState<boolean>(false);
 
@@ -26,6 +27,7 @@ function GroupSelect(props: GroupSelectProps) {
     props.setSelectedGroupId(+event.currentTarget.id);
     setIsUlVisible(!isUlVisible);
   };
+
   return (
     <Wrapper>
       <SelectBtn onClick={toggleSelect}>
@@ -35,19 +37,17 @@ function GroupSelect(props: GroupSelectProps) {
           {isUlVisible ? <IconSelectUp /> : <IconSelectDown />}
         </IconWrapper>
       </SelectBtn>
-      <Ul visible={isUlVisible}>
-        {groups.map((group) => (
-          <Li key={group.id} id={group.id.toString()} onClick={liClickHandler}>
-            <ColorCircle colorstring={group.color} />
-            {group.title}
-          </Li>
-        ))}
-      </Ul>
+      <Dropdown<IGroup>
+        isUlVisible={isUlVisible}
+        setIsUlVisible={setIsUlVisible}
+        liClickHandler={liClickHandler}
+        dataArray={groups}
+      />
     </Wrapper>
   );
 }
 
-export default GroupSelect;
+export default SelectGroup;
 
 const Wrapper = styled.div`
   margin-right: 7px;
@@ -69,7 +69,7 @@ const ColorCircle = styled.div<{ colorstring: string }>`
   min-height: 10px;
   border-radius: 50%;
   background-color: ${(props) => props.colorstring};
-  margin: 0 11px 0 5px;
+  margin: 8px;
 `;
 
 const TitleSpan = styled.span`
@@ -84,26 +84,4 @@ const IconWrapper = styled.div`
   display: flex;
   align-items: center;
   margin: 0 8px;
-`;
-
-const Ul = styled.ul<{ visible: boolean }>`
-  display: ${(props) => (props.visible ? "block" : "none")};
-  list-style: none;
-  padding: 4px;
-  position: absolute;
-  border-radius: 4px;
-  background-color: #ffffff;
-  box-shadow: 0px 8px 8px 0px rgba(29, 91, 132, 0.25);
-  max-width: 160px;
-`;
-
-const Li = styled.li`
-  display: flex;
-  align-items: baseline;
-  cursor: pointer;
-  &:hover {
-    background-color: #cae2fe9c;
-  }
-  padding: 3px 5px;
-  border-radius: 4px;
 `;
