e9e9695dc2938f7c46d797723f799e4de31dda0d
cheonyeji <ssaltuck@gmail.com>
2023-08-08 15:20:53 +0900

    - 호버 시 케밥 아이콘 표시 - ColorCircle 더블클릭 시 색상 변경 기능 추가 - TextSpan 더블클릭 시 내용 변경 기능 추가

--- src/components/group/GroupItem.tsx
+++ src/components/group/GroupItem.tsx
@@ -1,20 +1,106 @@
-import { IGroup } from "../../models/atoms";
+import { useState, useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
+
+import IconKebap from "../icons/IconKebap";
+import EditDropdown from "./EditDropdown";
+import EditText from "./EditText";
+import Dropdown from "../UI/Dropdown";
+import { IGroup, groupsState } from "../../models/atoms";
+import { IColor, colors, getColorItemById } from "../../models/colorArr";
 
 type IGroupItem = {
   item: IGroup;
 };
 
 function GroupItem({ item }: IGroupItem) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [isEditDropdownVisible, setEditIsDropdownVisible] = useState(false);
+
+  const setGroups = useSetRecoilState(groupsState);
+
+  const kebapClickHandler = () => {
+    setEditIsDropdownVisible(!isEditDropdownVisible);
+  };
+
+  const liClickHandler = (event: React.MouseEvent<HTMLElement>) => {
+    // +event.currentTarget.id 가지고 group의 요소 변경하기
+    const colorItemById = getColorItemById(+event.currentTarget.id);
+    // 해당 group id 전체 요소에서 찾아서 group Color 바꾸기
+    setGroups((prevGroups) => {
+      const findSameId = (element: IGroup, targetId: number) => {
+        if (element.id === targetId) {
+          return true;
+        }
+      };
+      const targetIndex = prevGroups.findIndex((element) =>
+        findSameId(element, item.id)
+      );
+
+      const modifiedGroup = {
+        id: prevGroups[targetIndex].id,
+        color: colorItemById!.color,
+        title: prevGroups[targetIndex].title,
+      };
+
+      const newState = [...prevGroups];
+      newState.splice(targetIndex, 1);
+      newState.splice(targetIndex, 0, modifiedGroup);
+
+      return newState;
+    });
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
   return (
-    <Item>
-      <ColorCircle colorstring={item.color} />
-      {item.title}
+    <Item
+      onMouseLeave={() => {
+        setEditIsDropdownVisible(false);
+      }}
+    >
+      <CircleTitleWrapper>
+        <ColorCircle
+          colorstring={item.color}
+          onDoubleClick={() => {
+            setIsDropdownVisible(!isDropdownVisible);
+          }}
+        />
+
+        <TextSpan onDoubleClick={() => setIsEdit(!isEdit)}>
+          {isEdit ? (
+            <EditText
+              text={item.title}
+              setIsEdit={setIsEdit}
+              itemId={item.id}
+            />
+          ) : (
+            item.title
+          )}
+        </TextSpan>
+      </CircleTitleWrapper>
+
+      <Dropdown<IColor>
+        isUlVisible={isDropdownVisible}
+        setIsUlVisible={setIsDropdownVisible}
+        liClickHandler={liClickHandler}
+        dataArray={colors}
+      />
+
+      <IconKebapWrapper onClick={kebapClickHandler}>
+        <IconKebap />
+        {isEditDropdownVisible && <EditDropdown />}
+      </IconKebapWrapper>
     </Item>
   );
 }
 
 export default GroupItem;
+
+const IconKebapWrapper = styled.div`
+  display: none;
+  cursor: pointer;
+`;
 
 const Item = styled.li`
   font-size: 12px;
@@ -23,6 +109,11 @@ const Item = styled.li`
   margin-bottom: 18px;
   margin-left: 13px;
   margin-right: 13px;
+  justify-content: space-between;
+
+  &:hover > ${IconKebapWrapper} {
+    display: block;
+  }
 
   @media (max-width: 1200px) {
     margin-bottom: 0px;
@@ -39,10 +130,23 @@ const Item = styled.li`
   }
 `;
 
+const CircleTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+`;
+
 const ColorCircle = styled.div<{ colorstring: string }>`
   min-width: 0.8em;
   min-height: 0.8em;
+  width: 0.8em;
+  height: 0.8em;
   border-radius: 50%;
   background-color: ${(props) => props.colorstring};
   margin-right: 8px;
+`;
+
+const TextSpan = styled.span`
+  width: 100%;
+  display: block;
 `;
