bee37958c5eedc7d58020001eee099d7270a6e26
cheonyeji <ssaltuck@gmail.com>
2023-08-08 15:09:53 +0900

    그룹아이템 호버시 등장하는 케밥아이콘 컴포넌트

---
+++ src/components/icons/IconKebap.tsx
@@ -0,0 +1,17 @@
+function IconKebap() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 22 104"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="11" cy="11" r="11" fill="#929292" />
+      <circle cx="11" cy="52" r="11" fill="#929292" />
+      <circle cx="11" cy="93" r="11" fill="#929292" />
+    </svg>
+  );
+}
+
+export default IconKebap;
