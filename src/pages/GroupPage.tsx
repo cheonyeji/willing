5b2c614268cbfa2e85f0eead4454b70c06aadac4
cheonyeji <ssaltuck@gmail.com>
2023-08-11 15:05:01 +0900

    드래그 끝났을 때 (dragEndHandler) - 완료 <-> 미완료 변경  - DEFAULT (groupId : 0)은 변경X - selectedGroupIdState 완료된 요소로 설정되지 않도록 설정
    - 드래그 중 completed 영역으로 이동할 때 atom 값 변경 (onDragUpdate)

--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -1,41 +1,95 @@
 import { styled } from "styled-components";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 import Groups from "../components/group/Groups";
 import NewGroup from "../components/group/NewGroup";
-import { groupsState } from "../models/atoms";
+import {
+  IGroup,
+  groupsState,
+  isOverCompletedState,
+  selectedGroupIdState,
+} from "../models/atoms";
 import { findSameId } from "../utils/RecoilFunctions";
 
 function GroupPage() {
-  const setGroups = useSetRecoilState(groupsState);
+  const [groups, setGroups] = useRecoilState(groupsState);
+  const setSelectedGroupIdState = useSetRecoilState(selectedGroupIdState);
+  const setIsOverCompleted = useSetRecoilState(isOverCompletedState);
+
+  const changeSelectedGroupId = (groupId: number, groups: IGroup[]) => {
+    setSelectedGroupIdState((prevGroupId) => {
+      if (prevGroupId === groupId) {
+        const targetIndex = groups.findIndex((group) => group.id === groupId);
+        return targetIndex > 0 && groups[targetIndex - 1]!.id !== -1
+          ? groups[targetIndex - 1]!.id
+          : 0;
+      } else {
+        return prevGroupId;
+      }
+    });
+  };
 
   const dragEndHandler = ({ destination, draggableId, source }: DropResult) => {
+    setIsOverCompleted(false);
+
     if (!destination) return;
 
     setGroups((allGroups) => {
-      const newGroups = [...allGroups];
+      const dedicatedGroup = { ...allGroups[0] };
       const sourceIndex = allGroups.findIndex((element) =>
         findSameId(element, +draggableId)
       );
-      const moveGroup = { ...newGroups[sourceIndex] };
+      const moveGroup = { ...allGroups[sourceIndex] };
 
-      if (destination.droppableId !== source.droppableId) {
+      const uncompletedGroups = allGroups.filter((group) => !group.completed);
+      const completedGroups = allGroups.filter(
+        (group) => group.completed && group.id !== -1
+      );
+
+      if (
+        destination.droppableId !== source.droppableId &&
+        +draggableId !== 0
+      ) {
         moveGroup.completed =
           destination.droppableId === "uncompletedGroups" ? false : true;
       }
 
-      const destinationIndex = allGroups.findIndex((element) =>
-        findSameId(element, allGroups[destination.index].id)
-      );
-      newGroups.splice(sourceIndex, 1);
-      newGroups.splice(destinationIndex, 0, moveGroup);
-      return newGroups;
+      if (source.droppableId === "uncompletedGroups") {
+        uncompletedGroups.splice(source.index, 1);
+      } else {
+        completedGroups.splice(source.index, 1);
+      }
+
+      if (destination.droppableId === "uncompletedGroups") {
+        uncompletedGroups.splice(destination.index, 0, moveGroup);
+      } else {
+        completedGroups.splice(destination.index, 0, moveGroup);
+      }
+
+      return [dedicatedGroup, ...uncompletedGroups, ...completedGroups];
     });
+
+    // selectedGroupIdState 요소가 만약 현재 group id인 경우 배열 받아와서 이전 데이터로 설정 (groupId : 0인 요소는 삭제불가라 무조건 존재)
+    if (
+      destination.droppableId !== source.droppableId &&
+      destination.droppableId === "completedGroups"
+    ) {
+      changeSelectedGroupId(+draggableId, groups);
+    }
   };
 
   return (
-    <DragDropContext onDragEnd={dragEndHandler}>
+    <DragDropContext
+      onDragEnd={dragEndHandler}
+      onDragUpdate={(update) => {
+        if (update.destination?.droppableId === "completedGroups") {
+          setIsOverCompleted(true);
+        } else {
+          setIsOverCompleted(false);
+        }
+      }}
+    >
       <Wrapper>
         <NewGroup />
         <Groups />
