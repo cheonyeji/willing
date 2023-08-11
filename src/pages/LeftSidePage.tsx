bc474a8a243d11f2cc55cc8fa9468e0a32158ddd
mijung858 <mimi5904@naver.com>
2023-08-11 17:28:17 +0900

    캘린더 ,메모 버튼레이아웃

--- src/pages/LeftSidePage.tsx
+++ src/pages/LeftSidePage.tsx
@@ -1,6 +1,8 @@
 import GroupPage from "./GroupPage";
 import ProfilePage from "./ProfilePage";
-
+import MemoBtn from "./MemoBtn";
+import CalenderBtn from "./CalenderBtn";
+import { styled } from "styled-components";
 type pageProps = { className: string };
 
 function SidebarPage(props: pageProps) {
@@ -8,8 +10,27 @@ function SidebarPage(props: pageProps) {
     <div className={props.className}>
       <ProfilePage />
       <GroupPage />
+      <BtnWrapper>
+        <MemoBtn />
+        <CalenderBtn />
+      </BtnWrapper>
     </div>
   );
 }
 
 export default SidebarPage;
+
+const BtnWrapper = styled.div`
+  position: absolute;
+  top: 89%;
+  left: 20px;
+  display: flex;
+  gap: 10%;
+  z-index: 999;
+  @media (max-width: 1200px) {
+    left: 30px;
+  }
+  @media (max-width: 768px) {
+    left: 30px;
+  }
+`;
