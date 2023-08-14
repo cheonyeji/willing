7ddcb50fb9928212eab8325e42d67eac5977160f
mijung858 <mimi5904@naver.com>
2023-08-14 14:13:34 +0900

    아이콘 컴플리트 생성

---
+++ src/components/icons/IconComplete.tsx
@@ -0,0 +1,22 @@
+function IconComplete() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="14" height="14" rx="4" fill="#ABCBFC" />
+      <rect width="14" height="14" rx="4" fill="#ABCBFC" />
+      <path
+        d="M2.82608 5.99133L5.79347 9.0435L10.7391 3.95654"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+export default IconComplete;
