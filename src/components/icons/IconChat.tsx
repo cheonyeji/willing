c953f134b284dc8c8f525af904d0b9a66808ef03
cheonyeji <ssaltuck@gmail.com>
2023-08-14 19:42:11 +0900

    js 유효 속성으로 변경 (camelCase)

--- src/components/icons/IconChat.tsx
+++ src/components/icons/IconChat.tsx
@@ -10,8 +10,8 @@ function IconCalender() {
       <path
         d="M7.5 9.83459V9.75M12 9.83459V9.75M16.5 9.83459V9.75M14.4457 16.3043L12 21L9.75 16.3043H5.25C4.00736 16.3043 3 15.297 3 14.0543V5.25C3 4.00736 4.00736 3 5.25 3H18.75C19.9926 3 21 4.00736 21 5.25V14.0543C21 15.297 19.9926 16.3043 18.75 16.3043H14.4457Z"
         stroke="white"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
