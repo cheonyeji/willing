8adc2dae014b99eec272298f89c0dc1b44b45a5d
cheonyeji <ssaltuck@gmail.com>
2023-08-14 19:44:43 +0900

    버튼 클릭 시 보여지는 모달 컴포넌트 (CSS 수정 필요)

---
+++ src/components/modal/CalendarModal.tsx
@@ -0,0 +1,12 @@
+import ShowCalendar from "../calendar/ShowCalendar";
+import Modal from "../UI/Modal";
+
+function CalendarModal() {
+  return (
+    <Modal>
+      <ShowCalendar />
+    </Modal>
+  );
+}
+
+export default CalendarModal;
---
+++ src/components/modal/MemoModal.tsx
@@ -0,0 +1,12 @@
+import MemoPage from "../../pages/MemoPage";
+import Modal from "../UI/Modal";
+
+function MemoModal() {
+  return (
+    <Modal>
+      <MemoPage />
+    </Modal>
+  );
+}
+
+export default MemoModal;
