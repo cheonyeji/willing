e9300c2c6eca17e1f0f179a4252e2fafece7f075
cheonyeji <ssaltuck@gmail.com>
2023-08-14 19:44:22 +0900

    - 1200 이하일때만 메모, 캘린더 버튼 표시되도록 반응형 수정 - 겹치는 코드 제거

--- src/pages/LeftSidePage.tsx
+++ src/pages/LeftSidePage.tsx
@@ -1,19 +1,34 @@
+import { styled } from "styled-components";
+import { useMediaQuery } from "react-responsive";
+
 import GroupPage from "./GroupPage";
 import ProfilePage from "./ProfilePage";
-import MemoBtn from "./MemoBtn";
-import CalenderBtn from "./CalenderBtn";
-import { styled } from "styled-components";
+import MemoBtn from "../components/modal/button/MemoBtn";
+import CalenderBtn from "../components/modal/button/CalenderBtn";
+import { useSetRecoilState } from "recoil";
+import { isModalShownState } from "../models/atoms";
+
 type pageProps = { className: string };
 
 function SidebarPage(props: pageProps) {
+  const setIsModalShown = useSetRecoilState(isModalShownState);
+  const isLess1200 = useMediaQuery({ maxWidth: 1200 });
+
+  if (!isLess1200) {
+    setIsModalShown(0);
+  }
+
   return (
     <div className={props.className}>
       <ProfilePage />
       <GroupPage />
-      <BtnWrapper>
-        <MemoBtn />
-        <CalenderBtn />
-      </BtnWrapper>
+
+      {isLess1200 && (
+        <BtnWrapper>
+          <MemoBtn />
+          <CalenderBtn />
+        </BtnWrapper>
+      )}
     </div>
   );
 }
@@ -28,9 +43,6 @@ const BtnWrapper = styled.div`
   gap: 10%;
   z-index: 999;
   @media (max-width: 1200px) {
-    left: 30px;
-  }
-  @media (max-width: 768px) {
     left: 30px;
   }
 `;
