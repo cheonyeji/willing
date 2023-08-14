f08f2b0b9637a29e7ef3313ea61431a21dc49232
mijung858 <mimi5904@naver.com>
2023-08-14 14:13:18 +0900

    아이콘 컴플리트 생성

---
+++ src/components/icons/IconCheck.tsx
@@ -0,0 +1,22 @@
+function IconCheck() {
+  return (
+    <svg
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="13" height="13" rx="4" fill="#ABCBFC" />
+      <rect width="13" height="13" rx="3" fill="#ABCBFC" />
+      <path
+        d="M2.82611 5.9913L5.7935 9.04347L10.7392 3.95651"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+export default IconCheck;
