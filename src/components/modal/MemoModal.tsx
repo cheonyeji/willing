679b90730e5a8d98f2fddf07241da8d10d339c47
cheonyeji <ssaltuck@gmail.com>
2023-08-17 17:30:25 +0900

    메모모달창 UI 구현

--- src/components/modal/MemoModal.tsx
+++ src/components/modal/MemoModal.tsx
@@ -1,12 +1,24 @@
-import MemoPage from "../../pages/MemoPage";
+import { styled } from "styled-components";
 import Modal from "../UI/Modal";
+import Memos from "../memo/Memos";
+import NewMemo from "../memo/NewMemo";
 
 function MemoModal() {
   return (
     <Modal>
-      <MemoPage />
+      <Wrapper>
+        <Memos />
+        <NewMemo />
+      </Wrapper>
     </Modal>
   );
 }
 
 export default MemoModal;
+
+const Wrapper = styled.div`
+  background-color: #ffffff;
+
+  width: 300px;
+  height: 300px;
+`;
